ea2e448fca8f60dd554481d9d4d396306b7bee71
Anton Osten <anton@ostensible.me>
2013-05-06 01:17:06 -0500

    JSON dumps along the way for better data security

--- process_lang.py
+++ process_lang.py
@@ -6,30 +6,37 @@ from langtools import get_words, seq_freqs
 
 def main():
     print('Working...')
+    # we'll be dumping everything into a JSON file along the way
+    lang_data = {}
+    file = open('{}.json'.format(args.fn), 'w')
+    
+    # get words
     words = get_words(open(args.fn))
+    lang_data['words'] = words
+    json.dump(lang_data, file)
     print('Got words, getting letters...')
     
     letter_freqs = seq_freqs(words, 1)
     # get the letters
     letters = [letter for letter in letter_freqs if letter != '-']
+    
+    lang_data['letters'] = letters
+    json.dump(lang_data, file)
     print('Got letters...')
     
     print('Calculating sequence frequencies. Go make yourself a hot drink because this will take a while...')
     seq_freqs_3 = seq_freqs(words, 3)
-    print('Done for three letter sequences. Doing four letter sequences. Get yourself another hot drink...')
+    lang_data['sequences'] = {'3': seq_freqs_3}
+    json.dump(lang_data, file)
+    
+    print('Done for three letter sequences. Doing four letter sequences. You might as well go out for a party or whatever, because this usually takes very long...')
     seq_freqs_4 = seq_freqs(words, 4)
+    lang_data['sequences']['4'] = seq_freqs_4
+    json.dump(lang_data, file)
     
     print('Done the frequencies, packing it up...')
-    
-    # assemble the language data
-    lang_data = {'words': words, 'letters': letters, 'sequences': {
-    '3': seq_freqs_3, '4': seq_freqs_4}}
-    
-    # dump it into a JSON file
-    file = open('{}.json'.format(args.fn), 'w')
-    json.dump(lang_data, file)
     file.close()
-    print('Did JSON dump...')
+    print('Wrote the JSON file...')
     print('Yay, all done!')
     
 if __name__ == '__main__':
